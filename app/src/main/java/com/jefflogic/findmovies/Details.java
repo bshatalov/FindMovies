6153ee246fe968e9c7a36292484ffb97405b64a8
bshatalov <bshatalov@mail.ru>
2019-09-19 18:31:52 +0300

    refactoring

--- app/src/main/java/com/jefflogic/findmovies/Details.java
+++ app/src/main/java/com/jefflogic/findmovies/Details.java
@@ -1,19 +1,32 @@
 package com.jefflogic.findmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Details extends AppCompatActivity {
 
-//    private TextView mTextViewAstr;
+    private TextView  mTextViewDetails;
+    private ImageView mImageViewDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details);
+
+        mTextViewDetails  = findViewById(R.id.textViewDetails);
+        mImageViewDetails = findViewById(R.id.imageViewDetails);
+
+        Intent intent = getIntent();
+        int imageId  = intent.getIntExtra(MainActivity.imageCode, 0);
+        int noteId = intent.getIntExtra(MainActivity.noteCode , 0);
+
+        mImageViewDetails.setImageResource(imageId);
+        mTextViewDetails.setText(noteId);
     }
 
 }
--- app/src/main/java/com/jefflogic/findmovies/MainActivity.java
+++ app/src/main/java/com/jefflogic/findmovies/MainActivity.java
@@ -11,6 +11,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int colorPrimary = 0xFF000000;
+    private static final int colorAccent = 0xFFFF0000;
+    //public static final String buttonCode = "BUTTON";
+    public static final String imageCode = "IMAGE";
+    public static final String noteCode = "NOTE";
+    public static final String MESS_TEXT_VIEW_NUM = "TEXT_VIEW_NUM";
+
     private Button mButtonDetailsAstr;
     private Button mButtonDetailsMale;
     private Button mButtonDetailsTerm;
@@ -21,32 +28,87 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextViewTerm;
     private TextView mTextViewKing;
 
+    private View.OnClickListener mOnClickListener;
+
+    private static Intent intentDetails;
+
+    /////////////////
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(R.id.btnDetailsAstr, mTextViewAstr.getCurrentTextColor());
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+    ...
+        if (savedInstanceState != null) {
+            String message = savedInstanceState.getString(MESS_KEY);
+            if (!TextUtils.isEmpty(message)) {
+                mMessageEditText.setText(message);
+            }
+        }
+    ...
+    }
+    /////////////////
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mButtonDetailsAstr = findViewById(R.id.btnDetailsAstr);
         mButtonDetailsMale = findViewById(R.id.btnDetailsMale);
         mButtonDetailsTerm = findViewById(R.id.btnDetailsTerm);
         mButtonDetailsKing = findViewById(R.id.btnDetailsKing);
-        mButtonDetailsAstr = findViewById(R.id.btnDetailsAstr);
 
         mTextViewAstr = findViewById(R.id.textViewAstr);
         mTextViewMale = findViewById(R.id.textViewMale);
         mTextViewTerm = findViewById(R.id.textViewTerm);
         mTextViewKing = findViewById(R.id.textViewKing);
 
-        mButtonDetailsAstr.setOnClickListener(new Button.OnClickListener() {
+        mOnClickListener = new Button.OnClickListener() {
             public void onClick(View v) {
-                mTextViewAstr.setTextColor(0x88FF0000);
-                openDetails();
+                openDetails((Button) v);
             }
-        });
+        };
+        mButtonDetailsAstr.setOnClickListener(mOnClickListener);
+        mButtonDetailsKing.setOnClickListener(mOnClickListener);
+        mButtonDetailsMale.setOnClickListener(mOnClickListener);
+        mButtonDetailsTerm.setOnClickListener(mOnClickListener);
+
+        if (intentDetails == null) {
+            intentDetails = new Intent(MainActivity.this, Details.class);
+        }
    }
 
-    private void openDetails() {
-        Intent intent = new Intent(MainActivity.this, Details.class);
-        startActivity(intent);
+    private void resetAllColors(){
+        mTextViewAstr.setTextColor(colorPrimary);
+        mTextViewMale.setTextColor(colorPrimary);
+        mTextViewTerm.setTextColor(colorPrimary);
+        mTextViewKing.setTextColor(colorPrimary);
     }
 
+    private void beforeDetails(
+            TextView textView
+            ,int imageId
+            ,int stringId
+    ) {
+        resetAllColors();
+        textView.setTextColor(colorAccent);
+        intentDetails.putExtra(imageCode, imageId);
+        intentDetails.putExtra(noteCode, stringId);
+    }
+
+    private void openDetails(Button button) {
+
+        if (button == mButtonDetailsAstr) {
+            beforeDetails(mTextViewAstr, R.drawable.pict_astr, R.string.note_astr);
+        } else if (button == mButtonDetailsMale) {
+            beforeDetails(mTextViewMale, R.drawable.pict_male, R.string.note_male);
+        } else if (button == mButtonDetailsTerm) {
+            beforeDetails(mTextViewTerm, R.drawable.pict_term, R.string.note_term);
+        } else if (button == mButtonDetailsKing) {
+            beforeDetails(mTextViewKing, R.drawable.pict_king, R.string.note_king);
+        }
+
+        startActivity(intentDetails);
+    }
 }
